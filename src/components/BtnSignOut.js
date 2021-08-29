866b9e014f6910b90d3f70f55cc2a1af55c6a791
MagicPojska <pojskicsafet@gmail.com>
2021-08-29 15:09:32 +0200

    Minor CSS edits

--- src/components/BtnSignOut.js
+++ src/components/BtnSignOut.js
@@ -6,9 +6,7 @@ function BtnSignOut() {
 
 
     return (
-        <div style={{
-            display: 'flex', justifyContent: 'flex-end', position: 'fixed', width: '100%', backgroundColor: '#FAFAFA', top: 0, borderBottom: 'solid 1px lightgray', zIndex: '10'
-        }}>
+        <div className='header'>
             <Button style={{ padding: '20px', fontSize: '15px', borderRadius: '0', fontWeight: '600' }} onClick={() => auth.signOut()}>Sign Out</Button>
         </div>
     )
